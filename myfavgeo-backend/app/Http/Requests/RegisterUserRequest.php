5673f1c937ea2419d3c0773228f6e01decd5d84d
Lucas Alves <lucas1901alves@gmail.com>
2025-12-29 14:37:13 -0300

    fix(user-request): fix register user request add authorize

--- myfavgeo-backend/app/Http/Requests/RegisterUserRequest.php
+++ myfavgeo-backend/app/Http/Requests/RegisterUserRequest.php
@@ -6,6 +6,11 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class RegisterUserRequest extends FormRequest
 {
+    public function authorize(): bool
+    {
+        return true;
+    }
+    
     public function rules(): array
     {
         return [

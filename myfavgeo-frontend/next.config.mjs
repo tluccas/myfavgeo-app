8fcc3e5958ad3099ea7ecd7d15e39647d80aa7ab
Lucas Alves <lucas1901alves@gmail.com>
2025-12-29 19:53:08 -0300

    chore(rewrites): setup next.js proxy and relax cookie security for local dev

--- myfavgeo-frontend/next.config.mjs
+++ myfavgeo-frontend/next.config.mjs
@@ -15,6 +15,14 @@ const nextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://127.0.0.1:8000/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
